5521eff4988fe5c4c486a4bdbdb299de485219dd
seonghwanjo <joseonghwan3021@gmail.com>
2022-03-30 15:30:47 +0900

    2.6 Styles Part

--- App.tsx
+++ App.tsx
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar animated={true} style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>14</Text>
+          <Text style={styles.description}>Rainy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>14</Text>
+          <Text style={styles.description}>Rainy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>14</Text>
+          <Text style={styles.description}>Rainy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>14</Text>
+          <Text style={styles.description}>Rainy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>14</Text>
+          <Text style={styles.description}>Rainy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>14</Text>
+          <Text style={styles.description}>Rainy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>14</Text>
+          <Text style={styles.description}>Rainy</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +52,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "gray",
+  },
+  city: {
+    flex: 1,
+    backgroundColor: "tomato",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+  weather: {
+    backgroundColor: "gray",
+  },
+  day: {
+    flex: 6,
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontWeight: "600",
+    fontSize: 178,
+  },
+  description: {
+    fontSize: 60,
+    marginTop: -30,
   },
 });
